d0e8d4dc32530317049369c8f9153eca689c2140
Julien Pagès <j.parkouss@gmail.com>
2017-05-10 08:34:12 +0200

    add configuration to send mail using gmail smtp server

--- init.el
+++ init.el
@@ -323,6 +323,10 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;; send mail configuration
+  (setq smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-service 587)
+
   ;; mail config
   (require 'org-mu4e)  ;; allow to capture links to mails
   (with-eval-after-load 'mu4e-alert
