fd41148ae77aa775387366e73821935fc8de29a5
Julien Pagès <j.parkouss@gmail.com>
2017-08-17 08:49:24 +0200

    customize the c++ layer

--- init.el
+++ init.el
@@ -44,7 +44,9 @@ values."
      python
      markdown
      yaml
-     c-c++
+     '((c-c++ :variables
+              c-c++-default-mode-for-headers c++-mode
+              c-c++-enable-clang-support t))
      ;; semantic
      cscope
      better-edition
