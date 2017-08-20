16e364e15089ced31ac64f2395b72fb80b5042fb
Julien Pagès <j.parkouss@gmail.com>
2017-08-20 08:35:34 +0200

    fix quotes in c-c++ configuration

--- init.el
+++ init.el
@@ -44,9 +44,9 @@ values."
      python
      markdown
      yaml
-     '((c-c++ :variables
-              c-c++-default-mode-for-headers c++-mode
-              c-c++-enable-clang-support t))
+     (c-c++ :variables
+             c-c++-default-mode-for-headers 'c++-mode
+             c-c++-enable-clang-support t)
      ;; semantic
      cscope
      better-edition
